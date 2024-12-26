5ada183515233af1b9257f2f35154554a504b7a5
Joris Grau <joris.grau2@gmail.com>
2024-12-26 13:27:54 +0100

    Moved gamespecific stuff out of framework

--- Scripts/WeaponSystem/AttackCommands/IAttackCommand.cs
+++ Scripts/WeaponSystem/AttackCommands/IAttackCommand.cs
@@ -1,26 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using JG.Flyweights;
 using UnityEngine;
 
 public interface IAttackCommand 
 {
     void Execute(Vector3 spawnPosition, Quaternion spawnRotation);
-}
-
-public class ProjectileCommand : IAttackCommand
-{
-
-    private readonly ProjectileSettings settings;
-
-    public ProjectileCommand(ProjectileSettings settings)
-    {
-        this.settings = settings;
-    }
-
-
-    public void Execute(Vector3 spawnPosition, Quaternion spawnRotation)
-    {
-        FlyweightFactory.Spawn(settings, spawnPosition, spawnRotation);
-    }
 }
--- Scripts/WeaponSystem/WeaponConfig.cs
+++ Scripts/WeaponSystem/WeaponConfig.cs
@@ -9,12 +9,11 @@ public enum WeaponType
 }
 
 [CreateAssetMenu(fileName = "WeaponConfig", menuName = "Gameplay/WeaponSystem/WeaponConfig", order = 1)]
-public class WeaponConfig : ScriptableObject
+public partial class WeaponConfig : ScriptableObject
 {
     public string weaponName;
     public float weaponDamage;
     public GameObject weaponPrefab;
     public WeaponType weaponType;
     public IAttackCommand attackCommand;
-    public ProjectileSettings projectileSettings;
 }
