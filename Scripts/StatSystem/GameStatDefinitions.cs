5db8b1ac2db1c95da2f528371c9971d7b53a5326
Joris Grau <joris.grau2@gmail.com>
2025-02-24 09:07:19 +0100

    Update GameStatDefinitions.cs

--- Scripts/StatSystem/GameStatDefinitions.cs
+++ Scripts/StatSystem/GameStatDefinitions.cs
@@ -10,5 +10,8 @@ public static class GameStatDefinitions
     public static StatDefinition MoveSpeed => StatRegistryProvider.Instance.GetStatDefinitionById(3);
     public static StatDefinition Range => StatRegistryProvider.Instance.GetStatDefinitionById(4);
     public static StatDefinition FireRate => StatRegistryProvider.Instance.GetStatDefinitionById(5);
+    public static StatDefinition DamageToCastle => StatRegistryProvider.Instance.GetStatDefinitionById(6);
+    public static StatDefinition RangedAttackRange => StatRegistryProvider.Instance.GetStatDefinitionById(7);
+    public static StatDefinition MeleeAttackRange => StatRegistryProvider.Instance.GetStatDefinitionById(8);
     // Add additional global stat definitions here as needed.
 }
