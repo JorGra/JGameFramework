fc8b7b6f4c95a06665901408ba5143d5200c7a1c
Joris Grau <joris.grau2@gmail.com>
2025-07-20 00:39:31 +0200

    Update Bootstrapper.cs

--- Scripts/SceneManagement/Bootstrapper.cs
+++ Scripts/SceneManagement/Bootstrapper.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using JG.Tools;
 #if UNITY_EDITOR
 using UnityEditor.SceneManagement;
 #endif
@@ -12,18 +10,45 @@ namespace JG.Tools.SceneManagement
 {
     public class Bootstrapper : Singleton<Bootstrapper>
     {
+
+        //#if !Unity_6
+        //        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        //        static IEnumerator Init()
+        //        {
+        //            Logger.Log("Bootstrapper Init...");
+        //            yield return SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+
+        //#if UNITY_EDITOR
+        //            // Set the bootstrapper scene to be the play mode start scene when running in the editor
+        //            // This will cause the bootstrapper scene to be loaded first (and only once) when entering
+        //            // play mode from the Unity Editor, regardless of which scene is currently active.
+        //            EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
+        //#endif
+        //        }
+        //#else
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        static IEnumerator Init()
+        private static void Init()          // <- static, void, no params
         {
-            Logger.Log("Bootstrapper Init...");
-            yield return SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+            Debug.Log("Bootstrapper Init...");
+
+            // If you really need it to be async you can still use
+            // LoadSceneAsync, just don’t try to *yield* here.
+            var op = SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+            // optional: hook a callback instead of yielding
+            // op.completed += _ => { /* do something after scene loaded */ };
+        }
 
 #if UNITY_EDITOR
-            // Set the bootstrapper scene to be the play mode start scene when running in the editor
-            // This will cause the bootstrapper scene to be loaded first (and only once) when entering
-            // play mode from the Unity Editor, regardless of which scene is currently active.
-            EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
-#endif
+        // --- Editor: runs when scripts reload (incl. before you hit Play) ---
+        [InitializeOnLoadMethod]            // Editor-time attribute
+        private static void SetStartScene()
+        {
+            var firstSceneAsset = AssetDatabase
+                                   .LoadAssetAtPath<SceneAsset>(
+                                        EditorBuildSettings.scenes[0].path);
+            EditorSceneManager.playModeStartScene = firstSceneAsset;
         }
+#endif
+        //#endif
     }
 }
